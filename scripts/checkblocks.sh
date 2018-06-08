428163435c5f6a65256163e8174eb451e98a1d44
Greerso <danny@greerso.com>
2018-06-08 00:16:35 -0400

    Checkblocks

---
+++ scripts/checkblocks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# checkdaemon.sh
+# Make sure the daemon is not stuck.
+# Add the following to the crontab (i.e. sudo crontab -e)
+# */30 * * * * ~/.bhash/checkdaemon.sh
+
+previousBlock=$(cat ~/.bhash/blockcount)
+currentBlock=$(bhash-cli getblockcount)
+
+bhash-cli getblockcount > ~/.bhash/blockcount
+
+if [ "$previousBlock" == "$currentBlock" ]; then
+  sudo systemctl restart bhashd
+fi
