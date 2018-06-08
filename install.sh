4134fa0cde23b459a51f2bd95ea75e90f11140c5
Greerso <danny@greerso.com>
2018-06-07 21:56:27 -0400

    maxconnections=32

--- install.sh
+++ install.sh
@@ -718,7 +718,7 @@ LOCAL_WALLET_CONF="rpcuser=$RPCUSER\nrpcpassword=$RPCPASSWORD\nrpcallowip=127.0.
 infobox "${INSTALL_STEPS[get_binaries]}"
     stfu download_binaries
 infobox "${INSTALL_STEPS[vps_configs]}"
-SERVER_WALLET_CONF="rpcuser=${RPCUSER}\nrpcpassword=${RPCPASSWORD}\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndaemon=1\nlogtimestamps=1\nmaxconnections=256\nmasternode=1\nexternalip=${PUBLIC_IP}\nmasternodeprivkey=${MN_PRIV_KEY}\ndatadir=${WALLET_LOCATION}"
+SERVER_WALLET_CONF="rpcuser=${RPCUSER}\nrpcpassword=${RPCPASSWORD}\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndaemon=1\nlogtimestamps=1\nmaxconnections=32\nmasternode=1\nexternalip=${PUBLIC_IP}\nmasternodeprivkey=${MN_PRIV_KEY}\ndatadir=${WALLET_LOCATION}"
     stfu wallet_configs
 infobox "${INSTALL_STEPS[vps_systemd]}"
 DAEMON_SERVICE="[Unit]\nDescription=$PROJECT_NAME daemon\nAfter=network.target\n\n[Service]\nExecStart=/usr/local/bin/$DAEMON_BINARY --daemon --conf=$WALLET_LOCATION/$PROJECT_NAME.conf -pid=/run/$DAEMON_BINARY/$DAEMON_BINARY.pid\nRuntimeDirectory=$DAEMON_BINARY\nUser=$LINUX_USER\nType=forking\nWorkingDirectory=$WALLET_LOCATION\nPIDFile=/run/$DAEMON_BINARY/$DAEMON_BINARY.pid\nRestart=on-failure\n\nPrivateTmp=true\nProtectSystem=full\nNoNewPrivileges=true\nPrivateDevices=true\nMemoryDenyWriteExecute=true\n\n[Install]\nWantedBy=multi-user.target"
